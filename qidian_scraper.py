dc0aaf525059ada672a365a732d52e9f8443159b
starfyre7 <starfyre7@gmail.com>
2017-07-28 20:04:33 -0400

    Grabs urls of chapters from url of table of contents

--- qidian_scraper.py
+++ qidian_scraper.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-
-
-import urllib2
+import requests
 from bs4 import BeautifulSoup
+import re
+import json
 
 #Grab URL and provide it instead of hard coding it
 url = "https://www.webnovel.com/book/6838665602003405/21799251029848595"
 
 
 def url_to_title_content(url_string):
-    page = urllib2.urlopen(url_string)
-    soup = BeautifulSoup(page)
+    page = requests.get(url_string)
+    soup = BeautifulSoup(page.text)
     title_div = soup.find(class_='cha-tit')
     title = title_div.find('h3').string
     
@@ -25,18 +25,21 @@ def url_to_title_content(url_string):
 #url_to_title_content(url)   
     
 toc_url = "https://www.webnovel.com/book/7141993106000405"
-def table_of_contents_scraper(toc_url):
-    page = urllib2.urlopen(toc_url)
-    soup = BeautifulSoup(page)
-    #print soup
-    pt = soup.find('div', class_='det-info')
-    print pt
-    title = soup.find("h2", class_=["mb10", "lh1d2", "oh"])
-    print title
-    toc = soup.find("ul", class_="g_mod_bd content-list")
-    print toc
-    
-    details = soup.find(class_='book-detail')
-    print details
 
-table_of_contents_scraper(toc_url)
+
+re_book_id = re.compile(r'webnovel.com/book/(\d*)/?.*')
+def table_of_contents_scraper(toc_url):
+    match = re.search(re_book_id, toc_url)
+    if not match:
+        print "Table of contents URL %s is invalid" %toc_url
+    book_id = match.group(1)
+    chapter_info = requests.get('https://www.webnovel.com/apiajax/chapter/GetChapterList?bookId=%s' %book_id)
+    js_ch = json.loads(chapter_info.text)
+    print js_ch['data']['bookInfo'] #temp print
+    
+    #May need to take in chapter index and sort if urls are not outputted in order as JSON is order agnostic
+    chapter_urls = ["https://www.webnovel.com/book/%s/%s" %(book_id, ch['chapterId']) for ch in js_ch['data']['chapterItems']]
+    return chapter_urls
+    
+
+print table_of_contents_scraper(toc_url)
